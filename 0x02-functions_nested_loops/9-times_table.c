0df164457a86ab9280cadb71365590ff539bf45b
Salma-samak <SALMA@example.com>
2023-08-15 14:55:26 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,14 +22,10 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-			{
 				putchar(' ');
-			}
 			else
-			{
 				putchar((prod / 10) + 48);
-			}
-			putchar((prod / 10) + 48);
+			putchar((prod % 10) + 48);
 		}
 		putchar('\n');
 	}
