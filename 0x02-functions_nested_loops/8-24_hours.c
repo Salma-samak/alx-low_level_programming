b04c0868f2e301ed65a4a8bef37d831c76869d15
Salma-samak <SALMA@example.com>
2023-08-15 14:29:22 -0700

    l

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+*/
+
+void jack_bauer(void)
+{
+	int main, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			putchar((hr / 10) + 48);
+			putchar((hr % 10) + 48);
+			putchar(':');
+			putchar((min / 10) + 48);
+			putchar((min % 10) + 48);
+			putchar('\n');
+		}
+	}
+}
