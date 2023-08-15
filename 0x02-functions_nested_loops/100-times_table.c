c8c1754e843772f2257fad131fb8af53c62cc63d
Salma-samak <SALMA@example.com>
2023-08-15 16:39:59 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - ptint time table of n
+ *
+ * @n: takes number input
+*/
+
+void print_times_table(int n)
+{
+	int prod, mult, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		putchar(48);
+		for (mult = 1; mult <= n; mult++)
+		{
+			putchar(',');
+			putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				putchar(' ');
+			if (prod <= 99)
+				putchar(' ');
+			if (prod >= 100)
+			{
+				putchar((prod / 100) + 48);
+				putchar((prod / 10) % 10 + 48);
+			} else if (prod <= 99 && prod >= 10)
+				putchar((prod / 10) + 48);
+			putchar((prod % 10) + 48);
+		}
+		putchar('\n')
+	}
+
+}
+
+
