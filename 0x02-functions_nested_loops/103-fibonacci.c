fd59aa444c922c78e0ad38a7f6ae56d5411ac841
Salma-samak <SALMA@example.com>
2023-08-16 08:27:04 -0700

    a

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#imclude "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: that finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long fib1, fib2, sum;
+	float totalsum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum >  4000000)
+			break;
+		if ((sum % 2) == 0)
+			totalsum += sum
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", totasum);
+
+	return (0);
+}
