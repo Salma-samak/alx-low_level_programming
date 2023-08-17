bb120c090fe4a90c0a937388af29f20943d6c029
Salma-samak <SALMA@example.com>
2023-08-17 11:16:53 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * @result: input
+ *
+ * Result: Always o
+*/
+
+void print_numbers(void)
+{
+	int result;
+
+	if (result >= 48 && result <= 57)
+		_putchar (result);
+	_putchar ("\n");
+	return (0);
+}
