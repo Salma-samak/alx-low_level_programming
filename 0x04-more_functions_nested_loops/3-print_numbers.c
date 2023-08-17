8dc40a9252676c28baa6257fe81c2539e6e1b99a
Salma-samak <SALMA@example.com>
2023-08-17 11:39:22 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
- * Result: Always o
+ * Result: Always 0
 */
 
 void print_numbers(void)
 {
-	int num = 0;
+	char c;
 
-	do {
-		putchar (num + 48);
-		num++;
-	} while (num >= 0 && num <= 9)
-	putchar ('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
 }
