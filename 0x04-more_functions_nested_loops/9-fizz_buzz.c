d76800f2f4f1dff2ee8c0f0cd2c857a370b17d22
Salma-samak <SALMA@example.com>
2023-08-18 04:44:13 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,15 +23,15 @@ int main(void)
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
 		} else
-		{
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
 			printf("\n");
-		}
-	}
+
 	printf("\n");
+	}
 
 	return (0);
 }
