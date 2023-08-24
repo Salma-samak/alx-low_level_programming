1162115c9479c9488cf8cbdc6122260a15834f16
Salma-samak <SALMA@example.com>
2023-08-24 16:19:39 -0700

    l

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,17 +11,17 @@ void print_number(int n)
 {
 	unsigned int i;
 
-	i = n;
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		n1 = -n;
 	}
 
-	if (i / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(i / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((i & 10) + '0');
+	_putchar((n1 & 10) + '0');
 }
