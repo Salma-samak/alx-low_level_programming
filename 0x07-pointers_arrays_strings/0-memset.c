c676a3f7c37f1ae69261892811f204b37fd90c7a
Salma-samak <SALMA@example.com>
2023-08-29 12:57:27 -0700

    k

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * *_memset - function that fills memory with a constant byte
- * @*s: pointer input
+ * @s: pointer input
  * @b: character
  * @n: max type of integer
  * Return: pointer s
@@ -12,9 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int Itr;
 
-	for (Itr = 0; n > 0; Itr++, n--)
+	for (Itr = 0; n > 0; Itr++)
 	{
 		s[Itr] = b;
+		n--;
 	}
 
 	return (s);
