20e18bede89c62c37ff0abb66c663b5160e3fdec
Salma-samak <SALMA@example.com>
2023-08-29 12:24:29 -0700

    k

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: pointer input
+ * @b: character
+ * @n: max type of integer
+ * Return: pointer s
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int c;
+
+	for (c = 0; n > 0; c++, n--)
+	{
+		s[c] = b;
+	}
+	return (s);
+}
