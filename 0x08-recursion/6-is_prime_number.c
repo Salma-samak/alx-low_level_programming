9a16dbada6f45f0c435769667fd17aabed5fcb74
Salma-samak <SALMA@example.com>
2023-08-31 14:24:48 -0700

    k

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_prime_number -  function that returns 1
+ * if the input integer is a prime number
+ *  otherwise return 0
+ *
+ * @n: input
+ *
+ * Return: 1 if n is prime, 0 if n not prime
+*/
+
+int is_prime_number(int n)
+{
+	if (n / n || 1 == 0)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
